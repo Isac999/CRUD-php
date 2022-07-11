2e93d523db68892cee09eda21129ee0e9c4b0750
Isac <isac.costa@fatec.sp.gov.br>
2022-07-11 09:48:40 -0300

    switch events

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,40 @@
-window.alert('Olaaaa');
+const selectCreate = document.querySelector('#select-table-create');
+const selectUpdate = document.querySelector('#select-table-update');
+const selectDelete = document.querySelector('#select-table-delete');
+const selectRead = document.querySelector('#select-table-read');
+
+selectCreate.addEventListener('change', (event) => {
+    choice(event);
+})
+selectUpdate.addEventListener('change', (event) => {
+    choice(event);
+})
+selectDelete.addEventListener('change', (event) => {
+    choice(event);
+})
+selectRead.addEventListener('change', (event) => {
+    choice(event);
+})
+
+function choice(param) {
+    switch(param.target.value) {
+        case 'books':
+            prompt('books');
+            break;
+        case 'customers':
+            prompt('cusmoters');
+            break;
+        case 'books_rentals':
+            //
+            break;
+        case 'requests_to_suppliers':
+            //
+            break;
+        case 'suppliers':
+            //
+            break;
+        case 'libraries':
+            //
+            break;
+    }
+}
