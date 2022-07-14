fe1b20a126190565826ee70b784bdb894ec3db6a
Isac <isac.costa@fatec.sp.gov.br>
2022-07-14 13:37:42 -0300

    update 5 tabs

--- admin/update.php
+++ admin/update.php
@@ -2,13 +2,15 @@
 include('./connect.php');
 header('Location: ./admin.php');
 
+$option = $_POST['option'];
+
 $id_target = $_POST['id-target'];
 $select_opt = $_POST['select-opt'];
 $new_value = $_POST['new-value'];
 
 $value = $mysqli->real_escape_string($new_value);
 
-$exec = "UPDATE `books` SET `$select_opt` = '$value' WHERE `books`.`id` = '$id_target'";
+$exec = "UPDATE `$option` SET `$select_opt` = '$value' WHERE `$option`.`id` = '$id_target'";
 $query = $mysqli->query($exec) or die('Falha ao executar update!');
 
 ?>
