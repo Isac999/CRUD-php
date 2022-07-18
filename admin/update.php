9f28f4038a757416bef2f70247fd6775be165cd5
Isac <isac.costa@fatec.sp.gov.br>
2022-07-18 10:36:25 -0300

    update changes

--- admin/update.php
+++ admin/update.php
@@ -1,20 +1,30 @@
 <?php 
 include('./connect.php');
 //header('Location: ./admin.php');
-/*
-$option = $_POST['option'];
 
-$id_target = $_POST['id-target'];
-$select_opt = $_POST['select-opt'];
-$new_value = $_POST['new-value'];
-
-$value = $mysqli->real_escape_string($new_value);
-
-$exec = "UPDATE `$option` SET `$select_opt` = '$value' WHERE `$option`.`id` = '$id_target'";
-$query = $mysqli->query($exec) or die('Falha ao executar update!');
-*/
-echo "antes";
 $data = file_get_contents('php://input');
-echo $data;
-echo "apos";
+$data = str_replace([":", "table", "}", "{"], "", $data);
+$list = explode("arrayValues", $data);
+
+$table_name = str_replace(['"', ','], "", $list[0]);
+$listData = str_replace(['"', '[', ']'], "", $list[1]);
+$listData = explode(',', $listData);
+
+echo $table_name;
+print_r($listData);
+
+$columns = array();
+$exec_columns = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'library' AND TABLE_NAME = '$table_name'";
+$selectColumns = $mysqli->query($exec_columns) or die('Erro ao consultar colunas');
+while ($column = $selectColumns->fetch_assoc()) {
+    array_push($columns, $column['COLUMN_NAME']);
+}
+
+print_r($columns);
+
+for ($index = 0; $index != count($columns); $index++) {
+    $exec = "UPDATE `$table_name` SET `$columns[$index]` = '$listData[$index]' WHERE `$table_name`.`id` = '$listData[0]'";
+    $query = $mysqli->query($exec) or die('Falha ao executar update!');
+}
+
 ?>
--- login.php
+++ login.php
@@ -91,7 +91,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     <div
     class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
     <div class="text-white mb-3 mb-md-0">
-      Copyright © 2021. All rights reserved.
+      Copyright © 2022. All rights reserved.
     </div>
     <div>
       <a href="#!" class="text-white me-4">
