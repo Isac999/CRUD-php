d9d4b772e2f928e7ac87a1ea2fce19e6f18267b8
Isac <isac.costa@fatec.sp.gov.br>
2022-07-22 12:22:00 -0300

    front changes

--- admin/admin.php
+++ admin/admin.php
@@ -57,11 +57,11 @@
     </div>
     </nav>
     <br> <br>
-    <div class="container mt-4 mb-4">
+    <div class="container mt-4 mb-4"><br>
         <div class="text-center">
             <h1 class="mt-1"> Welcome to the admin panel! </h1>
-            <p>Create, update, delete and read data quickly!</p>
-        </div>
+            <p>Create, update, delete and read data quickly!</p> 
+        </div><br>
         <div class="row justify-content-center">
             <table class="table table-striped">
                 <thead class="text-white font-weight-bold" style="background-color: #146176">
