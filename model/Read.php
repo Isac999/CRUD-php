d2b0244b62446be59ae1549df3566a8326a9ba54
Isac Fernandes <isac.costa@fatec.sp.gov.br>
2022-08-05 10:22:49 -0300

    url amigavel

--- model/Read.php
+++ model/Read.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION)) {
     session_start();
 } 
 if (!isset($_SESSION['id'])) {
-    header("Location: ./login.php");
+    header("Location: ./login");
     die("Você não tem acesso a está página! Faça login <a href='./login.php'>clicando aqui</a>");
 }
 
